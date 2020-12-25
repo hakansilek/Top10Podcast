512bcdfecf36782c7af1330177bcb31bfd781c36
Hakan Silek <silekhakan@gmail.com>
2020-12-26 01:20:56 +0300

    init

---
+++ app/src/main/java/com/sileksoftware/top10podcats/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.sileksoftware.top10podcats
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sileksoftware.top10podcats.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/sileksoftware/top10podcats/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.sileksoftware.top10podcats.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
