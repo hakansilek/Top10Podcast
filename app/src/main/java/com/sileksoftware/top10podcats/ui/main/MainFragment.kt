27d5ae9635e43b4e02b989866922e9bc27a91abd
Hakan Silek <silekhakan@gmail.com>
2020-12-31 08:43:08 +0300

    call top10podcast

--- app/src/main/java/com/sileksoftware/top10podcats/ui/main/MainFragment.kt
+++ app/src/main/java/com/sileksoftware/top10podcats/ui/main/MainFragment.kt
@@ -6,7 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.sileksoftware.top10podcats.R
+import com.sileksoftware.top10podcats.model.main.PodcastModel
 
 class MainFragment : Fragment() {
 
@@ -14,17 +19,35 @@ class MainFragment : Fragment() {
         fun newInstance() = MainFragment()
     }
 
+    private lateinit var podcastListRecyclerView: RecyclerView
     private lateinit var viewModel: MainViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        val v = inflater.inflate(R.layout.main_fragment, container, false)
+        podcastListRecyclerView = v.findViewById(R.id.podcastList)
+        podcastListRecyclerView.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL,false)
+
+        return v
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        getData()
+    }
+
+    private fun getData(){
+        val podcastObserve = Observer<List<PodcastModel>>{
+            podcastListRecyclerView.adapter = MainAdapter(it)
+        }
+
+        viewModel.podcastList.observe(this,podcastObserve)
+
+        viewModel.getTop10Podcast()
     }
 
 }
+
+
--- app/src/main/java/com/sileksoftware/top10podcats/ui/main/MainViewModel.kt
+++ app/src/main/java/com/sileksoftware/top10podcats/ui/main/MainViewModel.kt
@@ -1,7 +1,30 @@
 package com.sileksoftware.top10podcats.ui.main
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.sileksoftware.top10podcats.API.APIClient
+import com.sileksoftware.top10podcats.model.main.FeedModel
+import com.sileksoftware.top10podcats.model.main.PodcastModel
+import com.sileksoftware.top10podcats.service.main.PodcastService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    val podcastList = MutableLiveData<List<PodcastModel>>()
+    private val podcastService = APIClient.createService(PodcastService::class.java)
+
+    fun getTop10Podcast(){
+        podcastService.getTop10Podcasts().enqueue(object : Callback<FeedModel>{
+            override fun onResponse(call: Call<FeedModel>, response: Response<FeedModel>) {
+                if (response.isSuccessful){
+                    podcastList.value = response.body()?.feed?.results
+                }
+            }
+
+            override fun onFailure(call: Call<FeedModel>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+        })
+    }
 }
